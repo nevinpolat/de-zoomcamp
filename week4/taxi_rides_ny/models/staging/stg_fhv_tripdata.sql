022bc778a8f76a321b8c8bc2cdd81dfff143ea6c
Nevin Polat <npolat@live.co.uk>
2026-02-13 20:11:16 +0000

    fhv updated

--- week4/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
+++ week4/taxi_rides_ny/models/staging/stg_fhv_tripdata.sql
@@ -6,19 +6,12 @@ with source as (
 renamed as (
     select
         dispatching_base_num,
-        cast(PUlocationID as integer) as pickup_location_id,
-        cast(DOlocationID as integer) as dropoff_location_id,
-        cast(pickup_datetime as timestamp) as pickup_datetime,
-        cast(dropoff_datetime as timestamp) as dropoff_datetime,
-        cast(passenger_count as integer) as passenger_count,
-        cast(trip_distance as numeric) as trip_distance,
-        cast(fare_amount as numeric) as fare_amount,
-        cast(extra as numeric) as extra,
-        cast(mta_tax as numeric) as mta_tax,
-        cast(tip_amount as numeric) as tip_amount,
-        cast(tolls_amount as numeric) as tolls_amount,
-        cast(improvement_surcharge as numeric) as improvement_surcharge,
-        cast(total_amount as numeric) as total_amount
+        pickup_datetime,
+        dropOff_datetime as dropoff_datetime,
+        PUlocationID as pickup_location_id,
+        DOlocationID as dropoff_location_id,
+        SR_Flag as sr_flag,
+        Affiliated_base_number as affiliated_base_number
     from source
     where dispatching_base_num is not null
 )
